228367080e7300aa4cc563d7239c8324c92db8b6
Masaki Takehara <morisot64@gmail.com>
2019-07-16 00:30:06 +0900

    init firebase/config env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID
 };
+
