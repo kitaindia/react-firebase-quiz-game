c93683787b56856ae6b7fb8cf1c42ac138446bf0
Masaki Takehara <morisot64@gmail.com>
2019-08-12 16:30:24 +0900

    正解表示画面を変更

--- src/components/AnsDisplay.js
+++ src/components/AnsDisplay.js
@@ -5,14 +5,29 @@ class AnsDisplay extends Component {
     render() {
         const {currentQuiz} = this.props
 
+        const ansList = currentQuiz ? currentQuiz.answers.map((ansText, index) =>
+            <input
+                className={
+                  "screenPanel ansBtn "
+                  + ((index + 1) % 2 === 1 ? "fLeft " : "fRight ")
+                  + ((index === currentQuiz.ansIndex) ? "selected" : "")
+                }
+                key={index}
+                value={`${index + 1}. ${ansText}`}
+                type="button"
+                checked={index === 1}
+                style={{
+                    backgroundImage: "url('')",
+                    backgroundSize: "cover"
+                }}
+            />) : null;
+
         return (
             <div>
-                {currentQuiz &&
-                    <h1>
-                        {(currentQuiz.ansIndex + 1) + ". " +
-                            currentQuiz.answers[currentQuiz.ansIndex]}
-                    </h1>
-                }
+                {ansList && [
+                    <h1 key="ansTitle">正解は {`${currentQuiz.ansIndex + 1}. ${currentQuiz.answers[currentQuiz.ansIndex]}`}</h1>,
+                    <ol key="ansList"><h2>{ansList}</h2></ol>
+                ]}
             </div>
         )
     }
